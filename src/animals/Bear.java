a18f95fe77bd932fe6fbe54b64dbe593240465d7
Yoan Ballesteros <y.ballesteros@presta-module.com>
2017-10-06 11:42:47 +0200

    Add getters and setters for the class attributes

--- src/animals/Bear.java
+++ src/animals/Bear.java
@@ -29,8 +29,87 @@ public class Bear extends Animal implements Mammal {
     @Override
     public void eat() {
 
+    /**
+     * Allows to randomly generate a boolean, used for the sex definition in case of a 'natural' birth
+     * @return the randomly generated sex
+     */
+    public boolean getRandomBoolean() {
+        Random random = new Random();
+        return random.nextBoolean();
     }
 
+    /**
+     * Returns the sex of the current animal
+     * @return the current sex
+     */
+    public boolean getSex() {
+        return this.sex;
+    }
+
+    /**
+     * Sets the sex of the current animal
+     * @param sex
+     */
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * Returns the hunger level of the current animal
+     * @return the hunger level
+     */
+    public int getHunger() {
+        return this.hungerIndicator;
+    }
+
+    /**
+     * Allows to set the hunger level of the current animal
+     * @param newHungerLevel The desired hunger level to apply
+     */
+    public void setHunger(int newHungerLevel) {
+        this.hungerIndicator = newHungerLevel;
+    }
+
+    /**
+     * Returns the current health level
+     * @return the health level
+     */
+    public int getHealth() {
+        return this.healthIndicator;
+    }
+
+    /**
+     * Allows to update the health level
+     * @param newHealthLevel the new health level
+     */
+    public void setHealth(int newHealthLevel) {
+        this.healthIndicator = newHealthLevel;
+    }
+
+    /**
+     * Gets the current sleep state
+     * @return whether the animal sleeps or not
+     */
+    public boolean isSleeping() {
+        return this.sleepIndicator;
+    }
+
+    /**
+     * Allows to set the sleeping status to the one given in param
+     * @param status the new sleep state
+     */
+    public void setSleeping(boolean status) {
+        this.sleepIndicator = status;
+    }
+
+    /**
+     * Returns the turn number at which the copulation was done, allowing to compare with the current turn number
+     * in order to mesure the gestation
+     * @return the turn number
+     */
+    public int getCopulationTurn() {
+        return this.copulationTurn;
+    }
     @Override
     public void sound() {
 
