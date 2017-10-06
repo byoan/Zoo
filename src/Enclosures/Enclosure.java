d5267481c4d38d6a312a65ab39513608f06e231b
Sirven Antoine <antoinesirvenpro@gmail.com>
2017-10-06 11:32:21 +0200

    Create Enclosure Class/Interface

---
+++ src/Enclosures/Enclosure.java
@@ -0,0 +1,9 @@
+package Enclosures;
+
+public class Enclosure {
+    private String name;
+    private int surface;
+    private int maxAnimals;
+    private int nbAnimals;
+    private int cleanliness; //Percentage
+}
---
+++ src/Enclosures/EnclosureInterface.java
@@ -0,0 +1,16 @@
+package Enclosures;
+
+public interface EnclosureInterface {
+
+    public void statsEnclosure();
+
+    public void statsAnimal();
+
+    public void addAnimal();
+
+    public void deleteAnimal();
+
+    public void feedAnimùal();
+
+    public void cleanEnclosure();
+}
