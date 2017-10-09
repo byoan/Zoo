5e58e5249a2facb7d15b819ee78ade4b5720b8f5
Yoan Ballesteros <y.ballesteros@presta-module.com>
2017-10-09 17:06:11 +0200

    Remove useless redeclaration as we now use Generic typing

--- src/Enclosures/Aquarium.java
+++ src/Enclosures/Aquarium.java
@@ -7,8 +7,5 @@ import java.util.List;
 
 public class Aquarium<A extends MarineAnimal> extends Enclosure {
 
-    /**
-     * The collection of animals that are contained in the enclosure
-     */
-    private List<A> animals;
+
 }
--- src/Enclosures/Aviary.java
+++ src/Enclosures/Aviary.java
@@ -6,10 +6,6 @@ import java.util.List;
 
 public class Aviary<A extends FlyingAnimal> extends Enclosure {
 
-    /**
-     * The collection of animals that are contained in the enclosure
-     */
-    private List<A> animals;
 
 
 }
