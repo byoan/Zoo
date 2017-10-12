9a71ecba286903d272e165da4434871d119600d9
Yoan Ballesteros <y.ballesteros@presta-module.com>
2017-10-12 22:15:29 +0200

    Create a Factory for the Animals creation

---
+++ src/Core/AnimalFactory.java
@@ -0,0 +1,57 @@
+package Core;
+
+import animals.*;
+
+public final class AnimalFactory {
+
+    private static AnimalFactory instance = null;
+
+    private AnimalFactory() {
+        super();
+    }
+
+    public static AnimalFactory getInstance() {
+        if (AnimalFactory.instance == null) {
+            // synchronized allows use to keep the singleton even when using multiple threads
+            synchronized(AnimalFactory.class) {
+                if (AnimalFactory.instance == null) {
+                    AnimalFactory.instance = new AnimalFactory();
+                }
+            }
+        }
+        return AnimalFactory.instance;
+    }
+
+    public Bear createBear() {
+        return new Bear();
+    }
+
+    public Eagle createEagle() {
+        return new Eagle();
+    }
+
+    public Fish createFish() {
+        return new Fish();
+    }
+
+    public Pinguin createPinguin() {
+        return new Pinguin();
+    }
+
+    public Shark createShark() {
+        return new Shark();
+    }
+
+    public Tiger createTiger() {
+        return new Tiger();
+    }
+
+    public Whale createWhale() {
+        return new Whale();
+    }
+
+    public Wolf createWolf() {
+        return new Wolf();
+    }
+
+}
