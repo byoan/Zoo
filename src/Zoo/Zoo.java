89c0a552680e0f53f4a5bb95d072bc8a1277b30e
Yoan Ballesteros <y.ballesteros@presta-module.com>
2017-10-12 11:17:55 +0200

    initiate class Zoo

--- src/Zoo/Zoo.java
+++ src/Zoo/Zoo.java
@@ -1,4 +1,66 @@
 package Zoo;
 
+import Employees.Employee;
+import Enclosures.Enclosure;
+import animals.AnimalInterface;
+
+import java.util.ArrayList;
+
 public class Zoo {
+
+    private String nom;
+    private Employee employee;
+    private int maxNbEnclosure;
+    private ArrayList<Enclosure> enclosureList;
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Employee getEmployee() {
+        return this.employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getMaxNbEnclosure() {
+        return this.maxNbEnclosure;
+    }
+
+    public void setMaxNbEnclosure(int maxNbEnclosure) {
+        this.maxNbEnclosure = maxNbEnclosure;
+    }
+
+    public ArrayList<Enclosure> getEnclosureList() {
+        return this.enclosureList;
+    }
+
+    public void setEnclosureList(ArrayList<Enclosure> enclosureList) {
+        this.enclosureList = enclosureList;
+    }
+
+    public int getNbAnimalsInZoo() {
+        int nbAnimals = 0;
+        for (Enclosure enclosure : this.getEnclosureList()) {
+            nbAnimals += enclosure.getNbAnimals();
+        }
+        return nbAnimals;
+    }
+
+    public void displayAnimalsFromAllEnclosures() {
+        System.out.println("Displaying all animals currently in the Zoo : ");
+        for (Enclosure enclosure : this.getEnclosureList()) {
+            ArrayList<AnimalInterface> animalList = enclosure.getAnimals();
+            for (AnimalInterface animal : animalList) {
+                animal.toString();
+            }
+        }
+    }
+
 }
