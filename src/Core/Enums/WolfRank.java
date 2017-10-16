644c144e38b6f92543bf99759048a89fc1f50de4
Yoan Ballesteros <b.yoan@live.fr>
2017-10-16 19:55:02 +0200

    Enhance wolves rank list

--- src/Core/Enums/WolfRank.java
+++ src/Core/Enums/WolfRank.java
@@ -4,7 +4,30 @@ package Core.Enums;
  * Enum of all possible rank in a pack
  */
 public enum WolfRank {
-    Alpha, Beta, Gamma, Delta, Epsilon, Zêta, Eta, Thêta, Iota, Kappa, Lambda,
-    Mu, Nu, Xi, Omicron, Pi, Rhô, Sigma, Tau, Upsion, Phi, Chi, Psi, Omega,
+
+    Alpha,
+    Beta,
+    Gamma,
+    Delta,
+    Epsilon,
+    Zeta,
+    Eta,
+    Theta,
+    Iota,
+    Kappa,
+    Lambda,
+    Mu,
+    Nu,
+    Xi,
+    Omicron,
+    Pi,
+    Rhô,
+    Sigma,
+    Tau,
+    Upsilon,
+    Phi,
+    Chi,
+    Psi,
+    Omega
 
 }
