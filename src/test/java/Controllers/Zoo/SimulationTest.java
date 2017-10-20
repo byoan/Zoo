ab349fe53cbe4d8cfe27d58d53912f3e47f32e07
Yoan Ballesteros <b.yoan@live.fr>
2017-10-20 23:56:55 +0200

    Add tests for the Zoo and Simulation

--- src/test/java/Controllers/Zoo/SimulationTest.java
+++ src/test/java/Controllers/Zoo/SimulationTest.java
@@ -1,5 +1,6 @@
 package Controllers.Zoo;
 
+import Models.Employees.Employee;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimulationTest {
+
+    private Zoo zoo;
+    private Simulation simulation;
+
     @BeforeEach
     void setUp() {
+        this.simulation = new Simulation();
     }
 
     @AfterEach
@@ -16,11 +22,19 @@ class SimulationTest {
     }
 
     @Test
-    void getRandom() {
-    }
-
-    @Test
     void init() {
+        this.zoo = this.simulation.getZoo();
+
+        // Simple check that our main objects were properly instantiated
+        assertNotNull(this.simulation);
+        assertNotNull(this.zoo);
+
+        // Check that the turn number successfully initiated
+        assertEquals(0, this.simulation.getTurnNb());
+
+        // Check that we have enclosures and animals within them
+        assertNotEquals(0, this.zoo.getEnclosureList().size());
+        assertNotEquals(0, this.zoo.getNbAnimalsInZoo());
     }
 
 }
